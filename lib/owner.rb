a640ae61012bbea699226169ab74ab36f2c7478f
Panela Leung <leungpanela@gmail.com>
2019-09-15 03:31:35 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -85,6 +85,6 @@ class Owner
   end
 
     def list_pets
-      puts "I have #{self.dogs.each.count} dog(s), and #{self.cats.each.count} cat(s)."
+      puts "I have #{self.dog.each.count} dog(s), and #{self.cat.each.count} cat(s)."
     end
 end
