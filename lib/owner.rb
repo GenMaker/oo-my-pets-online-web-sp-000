f96b01e4bdd2a511b80956d7c38cdf8fec2e8360
Panela Leung <leungpanela@gmail.com>
2019-09-15 02:39:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -58,7 +58,7 @@ class Owner
 
   def sell_pets
     #binding.pry
-    Dog.all.each {|dog| dog.mood = "nervous"}
+    Dog.all.each {|dog| dog.mood = "nervous", owner.clear}
     @pets.all.each {|pet| pet.all.each {|mood| mood = "nervous"}}
   end
 end
