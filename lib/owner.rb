a3c7b305aab833fd413ba7bc81d9501ef6d010c7
Panela Leung <leungpanela@gmail.com>
2019-09-15 02:16:36 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -57,7 +57,7 @@ class Owner
   end
 
   def sell_pets
-    Dog.all.each {|dog| dog.mood = "nervous"}
+    Dog.all.each do {|dog| dog.mood = "nervous"}
     Cat.all.each {|cat| cat.mood = "nervous"}
   end
 end
